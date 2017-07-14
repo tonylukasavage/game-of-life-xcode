56b8d0efa9269d12d8d5db643a28dae53c555e3e
Tony Lukasavage <alukasavage@innovu.com>
2017-07-14 18:12:20 -0400

    Initial Commit

---
+++ Game Of Life/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game Of Life
+//
+//  Created by Tony Lukasavage on 7/14/17.
+//  Copyright © 2017 Tony Lukasavage. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
